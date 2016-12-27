46eda8e43e4ec803ef8404afc668da78f9c8e621
Nonobis <nonobis@gmail.com>
2016-12-27 10:32:51 +0100

    Fix UnitTest

--- SimpleNMATest/UnitTest1.cs
+++ SimpleNMATest/UnitTest1.cs
@@ -8,6 +8,16 @@ namespace SimpleNMATest
     public class UnitTest1
     {
         /// <summary>
+        /// The valid API key
+        /// </summary>
+        private string _ValidAPIKey = "0f4dba0bba3f9116c2ffc63064340fb6ca9baba393b727b5";
+
+        /// <summary>
+        /// The invalid API key
+        /// </summary>
+        private string _InvalidApiKey = "0f4dbafsba3f9116c2ffc63064340fb6ca9b42a393b727b5";
+
+        /// <summary>
         /// Checks the API success.
         /// </summary>
         [TestMethod]
@@ -15,16 +25,13 @@ namespace SimpleNMATest
         {
             try
             {
-                NMAManager.Instance.ApiKey = ""; // Set Valid API
+                NMAManager.Instance.ApiKey = _ValidAPIKey; // Valid Test API
                 var bResult = NMAManager.Instance.CheckApiKey();
-                if (bResult)
-                {
-                    Console.WriteLine("API OK");
-                }
+                Assert.IsTrue(bResult, "API Key Valid");
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Assert.Fail(ex.Message);
             }
         }
 
@@ -36,16 +43,12 @@ namespace SimpleNMATest
         {
             try
             {
-                NMAManager.Instance.ApiKey = ""; // Set Invalid API
-                var bResult = NMAManager.Instance.CheckApiKey();
-                if (bResult)
-                {
-                    Console.WriteLine("API OK");
-                }
+                NMAManager.Instance.ApiKey = _InvalidApiKey; // Random API for test
+                NMAManager.Instance.CheckApiKey();
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Assert.Fail(ex.Message);
             }
         }
 
@@ -57,18 +60,15 @@ namespace SimpleNMATest
         {
             try
             {
-                NMAManager.Instance.ApiKey = ""; // Set Valid API
-                NMAManager.Instance.ApplicationName = ""; // Set ApplicationName
+                NMAManager.Instance.ApiKey = _ValidAPIKey; // Set Valid API
+                NMAManager.Instance.ApplicationName = "SimpleNMA"; // Set ApplicationName
                 var oNotif = new NMANotification("Test", "Test Notification", PriorityNotification.Normal);
                 var bResult = NMAManager.Instance.Send(oNotif);
-                if (bResult)
-                {
-                    Console.WriteLine("Notification Send with Success");
-                }
+                Assert.IsTrue(bResult, "Notification Send with Success");
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Assert.Fail(ex.Message);
             }
         }
     }
